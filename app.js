2507c6b891406c27fe9a75e92cce36ae2ab4d5a1
Bridget27517 <149384116+Bridget27517@users.noreply.github.com>
2023-10-30 12:44:43 +0100

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_0jpha0o", "template_5qy74fz", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
